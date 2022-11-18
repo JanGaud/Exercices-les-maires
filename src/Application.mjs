decfe40f2ca47031a4a34965ec64ddbcff56f233
JanGaud <JanisGaudreault.jg@gmail.com>
2022-11-18 15:46:06 -0500

    Initialisation un peu retardataire de l'exercice 2 de programmation d'interfaceWeb

---
+++ src/Application.mjs
@@ -0,0 +1,70 @@
+import MaireXhr from "./MaireXhr.mjs";
+import MaireFetch from "./MaireFetch.mjs";
+
+export default class Application {
+     #btnListeNom = document.querySelectorAll(".btn-liste-nom");
+     #btnListeDate = document.querySelectorAll(".btn-liste-date")
+    /**
+     * Constructeur de la classe Application
+     * Le constructeur attache les écouteurs d'événement sur les bonnes méthodes de l'application
+     */
+    constructor() {
+        for(let i = 0; i < this.#btnListeNom.length; i++){
+          this.#btnListeNom[i].addEventListener("click", (event) => {this.liste(event, "nom")});
+        }
+
+        for(let i = 0; i < this.#btnListeDate.length; i++){
+          this.#btnListeDate[i].addEventListener("click", (event) => {this.liste(event, "date")});
+        }
+    }
+
+   
+    /**
+    Mettre les autres méthodes ici.
+    */
+    liste(event, type){
+      let fieldset = event.target.closest("fieldset");
+      let ordreTrie = fieldset.querySelector("input[name='ordre']:checked");
+      let params = {
+        type: type,
+        ordre: ordreTrie == null ? "asc" : ordreTrie.value,
+        callback: (data) => {this.render(data)}
+      };
+
+      if(fieldset.classList[0] == "xhr"){
+        MaireXhr.prototype.listeMaires(params);
+      }
+      
+      if(fieldset.classList[0] == "fetch"){
+        MaireFetch.prototype.listeMaires(params);
+      }    
+
+    }
+
+    render(data){
+      let tableauResultat = document.querySelector(".tbl-resultat");
+        tableauResultat.querySelector("tbody").innerHTML = "";
+      for(let i = 0; i < data.length; i++){
+        const row = document.createElement("tr");
+        const nom = document.createElement("td");
+        const prenom = document.createElement("td");
+        const debut = document.createElement("td");
+        const fin = document.createElement("td");
+
+        nom.innerHTML = data[i].nom;
+        prenom.innerHTML = data[i].prenom;
+        debut.innerHTML = data[i].debut;
+        fin.innerHTML = data[i].fin;
+
+
+        row.appendChild(nom);
+        row.appendChild(prenom);
+        row.appendChild(debut);
+        row.appendChild(fin);
+
+        
+        tableauResultat.querySelector("tbody").appendChild(row);
+      }
+    }
+
+  }
---
+++ src/MaireXhr.mjs
@@ -0,0 +1,88 @@
+/**
+ * Les routes de l'api
+ * url : https://jmartel.webdev.cmaisonneuve.qc.ca/wsmaires/maires/
+ * 
+ * Faire une recherche : /recherche
+ * ?type = [date, nom]
+ * ?valeur = chaine de recherche
+ * Ex 1 : https://jmartel.webdev.cmaisonneuve.qc.ca/wsmaires/maires/recherche?type=date&valeur=2012
+ * Ex 2 : https://jmartel.webdev.cmaisonneuve.qc.ca/wsmaires/maires/recherche?type=nom&valeur=Houde
+ * 
+ * Obtenir la liste : /liste
+ * ?tri=[date, nom]
+ * ?ordre=[asc, desc]
+ * Ex 3 : https://jmartel.webdev.cmaisonneuve.qc.ca/wsmaires/maires/liste?tri=date&ordre=asc
+ * Ex 4 : https://jmartel.webdev.cmaisonneuve.qc.ca/wsmaires/maires/liste?tri=nom&ordre=desc
+ * 
+ */
+
+
+export default class MaireXhr {
+    /**
+     * @typedef UnMaire
+     * @type {object}
+     * @property {string} nom - Le nom
+     * @property {string} prenom - Le prénom
+     * @property {number} debut - Année du début du mandat
+     * @property {?number} fin - Année de fin du mandat ou null
+     */
+
+    /**
+     * @type {UnMaire[]} Tableau des maires
+    }
+     */
+    #aMaires;
+    /**
+     * Représente le constructeur de la classe
+     * Qu'est-ce qui pourrait être fait rapidement à la construction de l'objet ?
+     */
+    constructor(textJSON){
+        let tableauParse = JSON.parse(textJSON);
+        this.#aMaires = Object.setPrototypeOf(tableauParse, MaireXhr.prototype);
+    }
+    
+     /**
+      * Retourne les résultats de la recherche
+      * @param {Object} params - Objet de paramètre
+      * @param {string} params.type - Le type de recherche : ["date", "nom"]
+      * @param {string | Number} params.valeur - La valeur recherchée
+      * @param {fonction} params.callback - La fonction de rappel, elle attend un paramètre, les données
+      * @returns {UnMaire[]} - Tableau des résultats;
+     */
+    rechercheMaires(params){
+        
+    }
+    
+    /**
+     * Retourne la liste des maires triée selon les paramètres
+     * @param {Object} params - Objet de paramètre
+     * @param {string} params.type - Le type de tri : ["date", "nom"]
+     * @param {string} params.ordre - L'ordre du tri : ["ASC", "DESC"]
+     * @param {fonction} params.callback - La fonction de rappel, elle attend un paramètre, les données
+     * @returns {UnMaire[]} - Tableau des résultats
+     */
+    listeMaires(params) {
+      
+      var oReq = new XMLHttpRequest();
+      oReq.onreadystatechange = () => {
+        if(oReq.readyState == 4 && oReq.status == 200){
+          params.callback(new MaireXhr(oReq.responseText).getMaire());
+        }
+      };
+
+      oReq.open("get", `https://jmartel.webdev.cmaisonneuve.qc.ca/wsmaires/maires/liste?tri=${params.type}&ordre=${params.ordre}`, true);
+      oReq.send();
+    }
+
+    /**
+     * @returns {Number} - Le nombre d'enregistrement dans le tableau des maires
+     */
+    getNombreMaires(){
+        
+    }
+
+    getMaire(){
+      return this.#aMaires
+    }
+    
+  }
